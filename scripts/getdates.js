3b0af778674bd2dc77c2bf26bd939592822a3573
Steven M. Tan <steven.tan@telus.com>
2025-05-08 14:27:21 -0400

    added week 1 project

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
